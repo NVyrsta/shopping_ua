a7801e96d0bcc8d878c59dc2a8dd4dfdcf9885a2
NVyrsta <natali.vyrsta@gmail.com>
2024-09-09 21:46:44 +0300

    fix(views): add cooperation page title

--- src/views/CooperationPage.vue
+++ src/views/CooperationPage.vue
@@ -11,120 +11,126 @@
         alt="Shopping UA" 
         class="absolute opacity-20 md:opacity-100 right-0 md:left-0 top-0 md:top-4 float-left md:float-none"
       >
-      
-      <div class="pl-0 md:pl-28 mt-3 ml-8 grid grid-cols-1 md:grid-cols-2 gap-4">
-        <div class="block gap-4 sm:flex">
-          <SvgIcon 
-            id="cooperation1"
-            width="91"
-            height="74"
-            fill="#3F3F3F"
-            class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
-          />
 
-          <p class="text-sm text-gray-600 leading-6">
-            {{ $t('Contacts.CooperationDescription1') }}
-          </p>
+      <div class="pl-0 md:pl-28 mt-3 ml-8">
+        <h2 class="text-red-500 font-semibold text-xl text-center mb-8">
+          {{ $t('Contacts.CooperationTitle') }}
+        </h2>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div class="block gap-4 sm:flex">
+            <SvgIcon 
+              id="cooperation1"
+              width="91"
+              height="74"
+              fill="#3F3F3F"
+              class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
+            />
+  
+            <p class="text-sm text-gray-600 leading-6">
+              {{ $t('Contacts.CooperationDescription1') }}
+            </p>
+          </div>
+  
+          <div class="block gap-4 sm:flex">
+            <SvgIcon 
+              id="cooperation5"
+              width="91"
+              height="74"
+              fill="#3F3F3F"
+              class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
+            />
+  
+            <p class="text-sm text-gray-600 leading-6">
+              {{ $t('Contacts.CooperationDescription5') }}
+            </p>
+          </div>
+  
+          <div class="block gap-4 sm:flex">
+            <SvgIcon 
+              id="cooperation2"
+              width="91"
+              height="74"
+              fill="#3F3F3F"
+              class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
+            />
+  
+            <p class="text-sm text-gray-600 leading-6">
+              {{ $t('Contacts.CooperationDescription2') }}
+            </p>
+          </div>
+  
+          <div class="block gap-4 sm:flex">
+            <SvgIcon 
+              id="cooperation6"
+              width="91"
+              height="74"
+              fill="#3F3F3F"
+              class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
+            />
+  
+            <p class="text-sm text-gray-600 leading-6">
+              {{ $t('Contacts.CooperationDescription6') }}
+            </p>
+          </div>
+  
+          <div class="block gap-4 sm:flex">
+            <SvgIcon 
+              id="cooperation3"
+              width="91"
+              height="74"
+              fill="#3F3F3F"
+              class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
+            />
+  
+            <p class="text-sm text-gray-600 leading-6">
+              {{ $t('Contacts.CooperationDescription3') }}
+            </p>
+          </div>
+  
+          <div class="block gap-4 sm:flex">
+            <SvgIcon 
+              id="cooperation7"
+              width="91"
+              height="74"
+              fill="#3F3F3F"
+              class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
+            />
+  
+            <p class="text-sm text-gray-600 leading-6">
+              {{ $t('Contacts.CooperationDescription7') }}
+            </p>
+          </div>
+  
+          <div class="block gap-4 sm:flex">
+            <SvgIcon 
+              id="cooperation4"
+              width="91"
+              height="74"
+              fill="#3F3F3F"
+              class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
+            />
+  
+            <p class="text-sm text-gray-600 leading-6">
+              {{ $t('Contacts.CooperationDescription4') }}
+            </p>
+          </div>
+  
+          <div class="block gap-4 sm:flex">
+            <SvgIcon 
+              id="cooperation8"
+              width="91"
+              height="74"
+              fill="#3F3F3F"
+              class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
+            />
+  
+            <p class="text-sm text-gray-600 leading-6">
+              {{ $t('Contacts.CooperationDescription8') }}
+            </p>
+          </div>
         </div>
-
-        <div class="block gap-4 sm:flex">
-          <SvgIcon 
-            id="cooperation5"
-            width="91"
-            height="74"
-            fill="#3F3F3F"
-            class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
-          />
-
-          <p class="text-sm text-gray-600 leading-6">
-            {{ $t('Contacts.CooperationDescription5') }}
-          </p>
-        </div>
-
-        <div class="block gap-4 sm:flex">
-          <SvgIcon 
-            id="cooperation2"
-            width="91"
-            height="74"
-            fill="#3F3F3F"
-            class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
-          />
-
-          <p class="text-sm text-gray-600 leading-6">
-            {{ $t('Contacts.CooperationDescription2') }}
-          </p>
-        </div>
-
-        <div class="block gap-4 sm:flex">
-          <SvgIcon 
-            id="cooperation6"
-            width="91"
-            height="74"
-            fill="#3F3F3F"
-            class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
-          />
-
-          <p class="text-sm text-gray-600 leading-6">
-            {{ $t('Contacts.CooperationDescription6') }}
-          </p>
-        </div>
-
-        <div class="block gap-4 sm:flex">
-          <SvgIcon 
-            id="cooperation3"
-            width="91"
-            height="74"
-            fill="#3F3F3F"
-            class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
-          />
-
-          <p class="text-sm text-gray-600 leading-6">
-            {{ $t('Contacts.CooperationDescription3') }}
-          </p>
-        </div>
-
-        <div class="block gap-4 sm:flex">
-          <SvgIcon 
-            id="cooperation7"
-            width="91"
-            height="74"
-            fill="#3F3F3F"
-            class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
-          />
-
-          <p class="text-sm text-gray-600 leading-6">
-            {{ $t('Contacts.CooperationDescription7') }}
-          </p>
-        </div>
-
-        <div class="block gap-4 sm:flex">
-          <SvgIcon 
-            id="cooperation4"
-            width="91"
-            height="74"
-            fill="#3F3F3F"
-            class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
-          />
-
-          <p class="text-sm text-gray-600 leading-6">
-            {{ $t('Contacts.CooperationDescription4') }}
-          </p>
-        </div>
-
-        <div class="block gap-4 sm:flex">
-          <SvgIcon 
-            id="cooperation8"
-            width="91"
-            height="74"
-            fill="#3F3F3F"
-            class="shrink-0 float-left sm:float-none mr-2 sm:mr-0"
-          />
-
-          <p class="text-sm text-gray-600 leading-6">
-            {{ $t('Contacts.CooperationDescription8') }}
-          </p>
-        </div>
-      </div>
+      </div>      
     </div>
 
     <div class="relative flex items-center justify-center bg-orange-100 py-20 overflow-hidden mt-16">
